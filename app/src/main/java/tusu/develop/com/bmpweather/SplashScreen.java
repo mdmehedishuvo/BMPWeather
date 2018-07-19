bb5883a3e11318d131cf51b5ea0d15ad75990630
Md Mehedi Hasan <bdmehedishuvo@gmail.com>
2018-07-19 23:40:59 +0600

    Some Pojo Class Added

--- app/src/main/java/tusu/develop/com/bmpweather/SplashScreen.java
+++ app/src/main/java/tusu/develop/com/bmpweather/SplashScreen.java
@@ -1,10 +1,21 @@
 package tusu.develop.com.bmpweather;
 
 import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 public class SplashScreen extends AppCompatActivity {
 
@@ -19,11 +30,96 @@ public class SplashScreen extends AppCompatActivity {
     //vars
     private Boolean mLocatiionsPermissionGranted=false;
     private GoogleMap map;
+    private FusedLocationProviderClient fusedLocationProviderClient;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        getLocationPermission();
+
+    }
+
+    private void getLocationPermission() {
+
+        Log.d(TAG, "getLocationPermission: getting Locations Permissions");
+        String[] permissions={Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.ACCESS_COARSE_LOCATION};
+
+        if (ContextCompat.checkSelfPermission(this.getApplicationContext(),FINE_LOCATIONS)== PackageManager.PERMISSION_GRANTED){
+
+            if (ContextCompat.checkSelfPermission(this.getApplicationContext(),COURSE_LOCATIONS)==PackageManager.PERMISSION_GRANTED){
+                mLocatiionsPermissionGranted=true;
+            }else {
+                ActivityCompat.requestPermissions(this,permissions,LOCATION_PERMISSIN_REQUEST_CODE);
+            }
+
+        }else {
+
+            ActivityCompat.requestPermissions(this,permissions,LOCATION_PERMISSIN_REQUEST_CODE);
+        }
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+
+        Log.d(TAG, "onRequestPermissionsResult: Called");
+        mLocatiionsPermissionGranted=false;
+
+        switch (requestCode){
+            case LOCATION_PERMISSIN_REQUEST_CODE:{
+                if (grantResults.length>0){
+                    for (int i=0;i<grantResults.length;i++){
+                        if (grantResults[i] !=PackageManager.PERMISSION_GRANTED){
+                            mLocatiionsPermissionGranted=false;
+                            Log.d(TAG, "onRequestPermissionsResult: Permission Faield");
+                            return;
+                        }
+                    }
+
+                    Log.d(TAG, "onRequestPermissionsResult: Permission Granred");
+                    mLocatiionsPermissionGranted=true;
+                }
+            }
+        }
+
+    }
+
+    private void getDeviceLocation(){
+
+        Log.d(TAG, "getDeviceLocation: getting the Device Current Locations");
+
+        fusedLocationProviderClient= LocationServices.getFusedLocationProviderClient(this);
+
+        try {
+
+            if (mLocatiionsPermissionGranted){
+
+                Task location=fusedLocationProviderClient.getLastLocation();
+
+                location.addOnCompleteListener(new OnCompleteListener() {
+                    @Override
+                    public void onComplete(@NonNull Task task) {
+                        if (task.isSuccessful()){
+
+                            Log.d(TAG, "onComplete: found Locations");
+                            Location currentLocation= (Location) task.getResult();
+                        }else {
+                            Log.d(TAG, "onComplete: current location is null");
+                            Toast.makeText(SplashScreen.this, "unable to get current locations", Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+                });
+
+            }
+
+
+        }catch (SecurityException n){
+            Log.d(TAG, "getDeviceLocation: "+n.getMessage());
+        }
+
     }
 }
